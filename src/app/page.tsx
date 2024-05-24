d5d1a1de9c2bed1c4000fecf177bb7b949e10cb1
Steve Korshakov <steve@korshakov.com>
2024-05-23 19:42:06 -0700

    feat: update button design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,27 @@ import color from 'color';
 function Button(props: { color: string }) {
   let parsed = color(props.color);
   let isDark = parsed.isDark();
-  let startColor = parsed.darken(0.1);
-  let endColor = parsed.lighten(0.2);
+  let gradient1 = parsed.lighten(0.2);
+  let gradient2 = parsed.lighten(0.1);
+  let gradient3 = parsed.lighten(0.05);
+  let gradient4 = parsed.darken(0.05);
   let shadowColor = isDark ? parsed.alpha(0.2) : color('black').alpha(0.1);
 
   // Dynamic style
   let style = {
-    background: `linear-gradient(${startColor.hex()}, ${endColor.hex()})`,
+    background: `linear-gradient(${gradient1.hex()} 0%, ${gradient2.hex()} 40%, ${gradient3.hex()} 70%, ${gradient4.hex()} 100%)`,
     boxShadow: `0 4px 4px ${shadowColor.rgb().string()}`,
     color: isDark ? 'white' : 'black',
   } satisfies React.CSSProperties;
 
   return (
     <button className='flex w-[128px] h-[48px] rounded-[24px] text-white justify-center items-center font-medium' style={style}>
-      {props.color}
+      <span>
+        btn{' '}
+        <span className='opacity-70'>
+          {props.color}
+        </span>
+      </span>
     </button>
   )
 }
